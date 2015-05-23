5571d8d3b68eef8538cdade4541e857d7eb204a8
Daryl Schroeder <daryl.schroeder@intergraph.com>
2015-05-23 06:41:41 +0000

    Changes made by Daryl Schroeder (daryl.schroeder@intergraph.com).

--- Matts Easy Pokemon Guessing Game/scripts/app.js
+++ Matts Easy Pokemon Guessing Game/scripts/app.js
@@ -13,21 +13,22 @@
         lookupPokemon: function (e, callback) {
             var request1 = new XMLHttpRequest(); //pokemon lookup...
             var xmlhttp2 = new XMLHttpRequest(); //image lookup...
-            
+
             //handler for pokemon return
             request1.onreadystatechange = function () {
                 if (request1.readyState == 4 && request1.status == 200) {
                     var myPokemon = JSON.parse(request1.responseText);
                     GAMECONTROLLER.set("currentPokemon", myPokemon);
+                    var v = GAMECONTROLLER.GeneratePickList();
                     xmlhttp2.open("GET", "http://pokeapi.co/" + myPokemon.sprites[0].resource_uri, true);
                     xmlhttp2.send();
                 }
             }
-            
+
             //handler for image return
             xmlhttp2.onreadystatechange = function () {
-                if (xmlhttp2.readyState == 4 && xmlhttp2.status == 200) {      
-                    var myPokemonSprite = JSON.parse(xmlhttp2.responseText);              
+                if (xmlhttp2.readyState == 4 && xmlhttp2.status == 200) {
+                    var myPokemonSprite = JSON.parse(xmlhttp2.responseText);
                     //alert(JSON.stringify(myPokemonSprite));
                     GAMECONTROLLER.set("currentPokemonSprite", myPokemonSprite);
                     GAMECONTROLLER.set("currentPokemonImageURL", "http://pokeapi.co/" + myPokemonSprite.image)
@@ -37,16 +38,64 @@
             request1.open("GET", this.pokeSearchURL + "pokemon/" + e, true);
             request1.send();
         },
+        GeneratePickList: function () {
+            //alert("wtf");
+            var curPokemon = this.currentPokemon.name;
+            //console.log(JSON.stringify(this.currentPokemon));
+            var falsePokemon1 = this.getRandomPokemonName();
+            var falsePokemon2 = this.getRandomPokemonName();
+            var unSorted = [];
+            unSorted.push(curPokemon);
+            unSorted.push(falsePokemon1);
+            unSorted.push(falsePokemon2);
+
+            console.log("three choices found: " + JSON.stringify(unSorted));
+
+            GAMECONTROLLER.set("currentPokemonChoices", this.shuffle(unSorted));
+        },
+        CheckAnswer: function(e)
+        {
+            console.log(e);
+            console.log(this.currentPokemon.name);
+          	if (e.toLowerCase() == this.currentPokemon.name.toLowerCase())  
+            { 
+                console.log("they match.");
+                return true;
+            }
+
+            console.log("they do not match.");
+            return false;
+        },
+        currentPokemonChoices: [],
+        shuffle: function (array) {
+            var currentIndex = array.length,
+                temporaryValue, randomIndex;
+
+            // While there remain elements to shuffle...
+            while (0 !== currentIndex) {
+
+                // Pick a remaining element...
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+
+                // And swap it with the current element.
+                temporaryValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = temporaryValue;
+            }
+
+            return array;
+        },
         getRandomPokemon: function (callback) {
             var index = Math.floor((Math.random() * this.pokemonGen1Count) + 1);
             this.lookupPokemon(index, callback);
         },
-        getRandomPokemonName: function(){
+        getRandomPokemonName: function () {
             var index = Math.floor((Math.random() * this.pokemonGen1Count) + 1);
             return this.pokemonGen1Name.split(",")[index];
-		}
+        }
     });
-    window.APP = kendo.observable( {
+    window.APP = kendo.observable({
         models: {
             home: {
                 title: 'Home',
@@ -54,37 +103,50 @@
                 CurrentPokemonImage: "images/Main.jpg",
                 CurrentPokemonAnswers: null,
                 CurrentPokemonCorrectIndex: null,
+                PokemonChoices: [],
+                PokemonSelected: null,
+                ResultStatusText: null,
+                CorrectCount:0,
+                IncorrectCount:0,
+                TotalCount:0,
+                GameStarted:false,
                 onPokemonFound: function (e) {
-                    if (e)
-					{
-                        
+                    if (e) {
+						if (!this.GameStarted)
+                        {
+                        	window.APP.set("models.home.GameStarted", true);                            
+						}
                         window.APP.set("models.home.CurrentPokemonImage", GAMECONTROLLER.currentPokemonImageURL);
                         window.APP.set("models.home.StatusText", "Who is this pokemon?");
-                        
-                        //get 2 false answers.
-                        var falseAnswers;
-                        falseAnswers.push(GAMECONTROLLER.getRandomPokemonName())
-                        falseAnswers.push(GAMECONTROLLER.getRandomPokemonName());
-                        
-                        //randomize the order:
-                        var correctIndex = Math.floor((Math.random() * 3) + 1);
-                        var tempOrder;
-                        for (int i = 0; i ++ ; i < 3)
-                        {
-                            if (i == correctIndex){
-                                tempOrder.push(GAMECONTROLLER.currentPokemon.name);
-                            }
-                            else 
-						    {
-                                tempOrder.push(falseAnswers.pop());
-							}
-						}
-                        alert(JSON.Parse(tempOrder));
-                        
-					};
+                        window.APP.set("models.home.PokemonChoices", GAMECONTROLLER.currentPokemonChoices);
+                        window.APP.set("models.home.PokemonSelected", GAMECONTROLLER.currentPokemonChoices[0]);
+                        console.log("PokemonSelected set to ", GAMECONTROLLER.currentPokemonChoices[0]);
+                        //alert(JSON.stringify(GAMECONTROLLER.currentPokemonChoices));
+                    };
                 },
                 listener: function (e) {
+                    //if game is ongoing, check if it's the correct answer.
+                    if (GAMECONTROLLER.currentPokemon != null)
+                    {	
+                        
+                        this.set("TotalCount", this.TotalCount+1);
+                        this.set("CurrentPokemonImage", "images/loading.gif");
+                        console.log("user selected: " + this.PokemonSelected);
+                        var isCorrect = GAMECONTROLLER.CheckAnswer(this.PokemonSelected);
+                        console.log("do they match?" + isCorrect);
+                        if (isCorrect)
+                        {
+                        	this.set("CorrectCount", this.CorrectCount+1);
+                            this.set("ResultStatusText", "Correct!");
+                        }
+                        else 
+                        {
+                        	this.set("IncorrectCount", this.IncorrectCount+1);
+                            this.set("ResultStatusText", "Incorrect, " + GAMECONTROLLER.currentPokemon.name);
+						}
+					}
                     GAMECONTROLLER.getRandomPokemon(this.onPokemonFound);
+                    
                 }
             },
             settings: {
